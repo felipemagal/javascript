963fb9ab12350dc05466e805a55817be5763d130
felipemagal <felipe19magal@outlook.com>
2022-12-10 17:58:14 -0300

    Data Types
    Fundamentos do JavaScript

---
+++ exercicios/ex003/script003.js
@@ -0,0 +1,19 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994;
+console.log(year);
+
+console.log(typeof null);

e20ffe148337289097f2591f8b13ad0394bdf670
felipemagal <felipe19magal@outlook.com>
2022-12-09 22:17:29 -0300

    Values and Variables
    Início de teoria sobre Valores e Variáveis com exercício prático

---
+++ exercicios/ex001/script001.js
@@ -0,0 +1,22 @@
+let js = "Amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Publicitário'
+let myCurrentJog = 'Estudante'
+
+console.log(myFirstJob);

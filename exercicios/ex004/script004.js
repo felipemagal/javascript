ed4d7a796a2e28b682c49dadfd34885f13441afc
felipemagal <felipe19magal@outlook.com>
2022-12-10 18:46:23 -0300

    Update script004.js

--- exercicios/ex004/script004.js
+++ exercicios/ex004/script004.js
@@ -6,11 +6,11 @@ age = 31;
 //const é usado para variáveis que não podem mudar. Como o ano de nascimento de uma pessoa. A linha 9 do exemplo abaixo está errada.
 
 const birthYear = 1994;
-birthYear = 1995;
+// birthYear = 1995;
 
 // Quando usamos const, precisamos basicamente de um valor inicial. O exemplo abaixo também está errado. O exemplo correto seria assim - "const job = "coder""
 
-const job;
+// const job;
 
 // Atualmente não é recomendando que se use a declaração var. Mas para efeitos práticos, basicamente, var e let tem a mesma função.
 

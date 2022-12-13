0cf9ad3cd149495cedb24166fcc5b8ef60ce8cdb
felipemagal <felipe19magal@outlook.com>
2022-12-12 21:01:37 -0300

    1 desafio de JS

---
+++ exercicios/desafio001/script-desafio.js
@@ -0,0 +1,14 @@
+const heightMark = 1.69;
+const weightMark = 78;
+
+heightMark ** 2
+console.log(heightMark ** 2)
+
+weightMark / heightMark ** 2
+console.log(weightMark / heightMark ** 2)
+
+const heightJohn = 1.95;
+const weightJohn = 92;
+
+console.log(weightJohn / heightJohn ** 2)
+

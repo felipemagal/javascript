8a9c59577432418410e1ffb46ec45237d56fa9ba
felipemagal <felipe19magal@outlook.com>
2023-01-16 12:42:05 -0300

    Update scriptdesafio003.js

--- exercicios/desafio003/scriptdesafio003.js
+++ exercicios/desafio003/scriptdesafio003.js
@@ -12,14 +12,30 @@
 
 // BONUS 1
 
-const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 123) / 3;
+// const scoreDolphins = (97 + 112 + 101) / 3;
+// const scoreKoalas = (109 + 95 + 106) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//     console.log('Dolphins win the trophy');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//     console.log('Koalas win the trhophy');
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy!')
+// }
+
+// BONUS 2
+
+const scoreDolphins = (97 + 112 + 81) / 3;
+const scoreKoalas = (109 + 95 + 86) / 3;
 console.log(scoreDolphins, scoreKoalas);
 
 if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
     console.log('Dolphins win the trophy');
 } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
     console.log('Koalas win the trhophy');
-} else if (scoreDolphins === scoreKoalas) {
-    console.log('Both win the trophy!')
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy!');
 }

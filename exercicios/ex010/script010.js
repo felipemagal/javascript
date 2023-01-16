9735539b442999215a19eb73cbe060fafdfe81a5
felipemagal <felipe19magal@outlook.com>
2023-01-16 13:06:43 -0300

    Switch Statemant

---
+++ exercicios/ex010/script010.js
@@ -0,0 +1,24 @@
+cont day = 'monday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course sttruture');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code exemples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}

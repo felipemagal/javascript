4e10d64b12ed4e4f70c41ebd18c670373696be48
felipemagal <felipe19magal@outlook.com>
2023-01-17 11:19:47 -0300

    Expressões e Declarações
    Exemplificações

---
+++ exercicios/ex011/script011.js
@@ -0,0 +1,14 @@
+// Exemplos de expressões - expressões produzem valores
+
+3 + 4
+1994
+true && false && !false
+
+// Exemplos de Declarações - declarações não produzem valores por si mesmo
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+
+const me = 'Felipe;'
+console.log(`i'm ${2023 - 1994} years old ${me}`)

490cc5e83a5aa42a8202b0c12b1b9ab0c397e556
felipemagal <felipe19magal@outlook.com>
2023-05-02 20:56:04 -0300

    Object Methods
    Isso aqui é bem confuso

--- exercicios/ex021/script021.js
+++ exercicios/ex021/script021.js
@@ -6,7 +6,33 @@ const felipe = {
     friends: ['Dani', 'Henrique', 'Toscano'],
     hasDriversLicense: true,
 
-    calcAge: function (birthYear) {
-        return 2037 - birthYear;
+    // calcAge: function (birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function () {
+    //     console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()} year old ${felipe.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
     }
 };
+
+console.log(felipe.calcAge(1994));
+// // console.log(felipe['calcAge'](1994));
+
+console.log(felipe.age);
+console.log(felipe.age);
+console.log(felipe.age);
+console.log(felipe.age);
+
+// Challenge
+// "Felipe is a 46 years old teacher, and he has a driver's license"
+
+console.log(felipe.getSummary());

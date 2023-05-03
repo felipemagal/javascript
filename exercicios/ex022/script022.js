2be3f1b120f1efb3b60889501160ce0a9f451c3e
felipemagal <felipe19magal@outlook.com>
2023-05-03 20:34:56 -0300

    for Loop

---
+++ exercicios/ex022/script022.js
@@ -0,0 +1,19 @@
+// No lugar de escrever isso
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// Escrevemos assim
+
+// Loop 'for' continua em execução enquanto a condição for verdadeira.
+for (let rep = 1; rep <= 10; rep++) { //rep++ é usado para que o loop quando acabar, inicie novamente
+    console.log(`Lifting weights repetition ${rep}`);
+}

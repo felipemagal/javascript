4df38e1137c6a81d764568c1eac6bb670baad136
felipemagal <felipe19magal@outlook.com>
2023-08-07 20:28:47 -0300

    Regras de const

---
+++ CursoJS/aula 7/index.js
@@ -0,0 +1,15 @@
+// REGRAS DE VARÁVEIS
+
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não podemos começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Não podemos modificar o valor de uma constante
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(primeiroNumero + segundoNumero);
+console.log(typeof primeiroNumero);

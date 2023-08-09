a0e663d2b676a0f0ca5446be92dc88c3575a58f0
felipemagal <felipe19magal@outlook.com>
2023-08-09 20:58:25 -0300

    Tipos de Dados Primitivos

---
+++ CursoJS/aula09/index.js
@@ -0,0 +1,12 @@
+// TIPO DE DADOS PRIMITIVOS - string, number, null, boolean, undefined e symbol
+
+const nome = "Felipe"; // string
+const nome1 = "Felipe"; //string
+const nome2 = `Felipe`; //string
+const num1 = 10; //number
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; //nulo -> não apont para local nenhum na memória
+const aprovado = false; //boolean -> true ou false (lógico)
+
+console.log(typeof num1);

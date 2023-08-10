d418ea01e747338826f80b0113f4f8164720f124
felipemagal <felipe19magal@outlook.com>
2023-08-09 21:33:33 -0300

    Operadores Aritméticos

---
+++ CursoJS/aula10/index.js
@@ -0,0 +1,22 @@
+//OPERADORES ARITMÉTICOS
+
+// + Adição / Concatenação
+// - Subtração
+// / Divisão
+// * Multiplicaçã0
+// ** Ponteciação
+// % Resto da divisão
+// ++ Incremento
+// -- Decremento
+
+// const num1 = 2;
+// const num2 = 10;
+// console.log(num1 ** num2);
+
+// const num1 = 2;
+// const num2 = parseInt("5"); // transforma o string em número inteiro
+// console.log(num1 * num2);
+
+const num1 = 2;
+const num2 = parseFloat("5.2"); //  o valor em número contabiliza as casas décimais
+console.log(num1 * num2);

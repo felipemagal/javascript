da681a30c11e977792aba6b9a77383926ff7b657
felipemagal <felipe19magal@outlook.com>
2023-12-04 20:58:09 -0300

    Ex

---
+++ CursoJS/aula14/index.js
@@ -0,0 +1,5 @@
+let num1 = 1;
+let num2 = 2.5;
+console.log(num1.toString() + num2); //toString converte temporariamente o número em string
+num1 = num1.toString();
+console.log(typeof num1, typeof num2);

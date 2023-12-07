78fbc2fe12de8cf71b419a5d3104cfb74acb97f2
felipemagal <felipe19magal@outlook.com>
2023-12-07 20:59:08 -0300

    Python
    Intrudução

---
+++ CursoJS/aula15/index.py
@@ -0,0 +1,38 @@
+# a=1
+# b=2
+# c=3
+# d= a+b+c
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print('isto é uma string')
+# print("isto é uma string")
+
+# print(4.12, int(4.14))
+# print(int("22"))
+
+# print(float("123.45"))
+# print(type(float("123.45")))
+
+# dia = "quinta-feira"
+# print(dia)
+# dia = "sexta-feira"
+# print(dia)
+# dia = 21
+# print(dia)
+
+# minutos = 645
+# horas = minutos // 60
+# print(horas)
+
+# quociente = 7 //3
+# print(quociente)
+# resto = 7 % 3
+# print(resto)
+
+totalSeg = 7684
+horas = totalSeg // 3600
+# segRestante =  totalSeg % 3600
+# minutos = segRestante // 60
+# segRestanteFinal = segRestante % 60

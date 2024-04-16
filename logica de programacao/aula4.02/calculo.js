53e6c7a7e4365fdba58a93bf59b9398c0eb5dda7
felipemagal <felipe19magal@outlook.com>
2024-04-15 23:06:29 -0300

    Cálculo de área
    Uso de funções para calcular

---
+++ logica de programacao/aula4.02/calculo.js
@@ -0,0 +1,11 @@
+var largura = prompt('Digite a largura');
+largura = parseFloat(largura);
+
+var altura = prompt('Digite a altura');
+altura = parseFloat(altura);
+
+
+
+function calcularArea(l, a) {
+    alert(`área total é ${l * a}`);
+}
